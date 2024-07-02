3624322c7dc089943cc424ec23b937e94ef7a0ab
CarlosMBarboza <michaelAlexander81@hotmail.com>
2024-07-02 20:06:55 -0300

    form

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -2,17 +2,23 @@
 
 const formularioLogin=  (req, res) => {
   res.render('auth/login',{ 
-
+    pagina: 'Inicia Sesion'
   })};
 
   const formularioRegistro=  (req, res) => {
     res.render('auth/registro',{ 
       pagina: 'Crea tu Cuenta'
     })};
-
-
+    const formularioOLvidePassword=  (req, res) => {
+      res.render('auth/olvidePassword',{ 
+        pagina: 'Recupera tu Cuenta'
+      })}
+  
 
     export {
       formularioLogin,
       formularioRegistro,
+      formularioOLvidePassword,
+     
+    
     }
