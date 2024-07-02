12681df8ecaaf7bf95155bd1b02a795e69a78099
CarlosMBarboza <michaelAlexander81@hotmail.com>
2024-07-02 20:07:27 -0300

    terminando los formularios

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,12 +1,15 @@
 import express from "express";
-import { formularioLogin } from "../controllers/usuarioController.js";
-import { formularioRegistro } from "../controllers/usuarioController.js";
+import { formularioLogin, formularioRegistro, formularioOLvidePassword } from "../controllers/usuarioController.js";
+
+
 
 
 const router = express.Router();
 
 router.get('/login', formularioLogin);
 router.get('/registro', formularioRegistro);
+router.get('/olvidePassword, formularioOLvidePassword');
+
 
 
 
